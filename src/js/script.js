1da35b01a55491577ad3cf3017f89baf09617da6
Shujinko53 <shujinko53@yandex.ru>
2021-03-15 20:24:10 +0300

    stylization and adapting

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,14 @@
-$(document).ready(function () {
-    const menu = document.querySelector('.header__menu'),
-        menuItem = document.querySelectorAll('.header__menu-item'),
-        hamburger = document.querySelector('.hamburger');
+$(document).ready(function() {
+    $('.hamburger').on('click', function() {
+        $('.hamburger').toggleClass('hamburger_active');
+        $('.header__menu').toggleClass('header__menu_active');
+    })
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 620){
+            $(".hamburger span").css("background-color","#a38c60");
+        } else if ($(window).scrollTop() < 620) {
+            $(".hamburger span").css("background-color","white");
+        }
     });
 })
